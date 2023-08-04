51caf653a7c187b15b36ffa6e5944cb209d053a4
AngelDittrich <rodrigo.diaz0102@gmail.com>
2023-08-04 11:39:41 -0600

    colors

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var miBoton = document.getElementById("miBoton");
+  
+
+    miBoton.addEventListener("click", function() {
+  
+      alert("¡Has hecho clic en el botón!");
+    });
+  });
+  
